cba34963dc4efdbbf15d2ed452b726835f52febc
Mathieu POUX <mathieu.poux@gmail.com>
2018-01-20 10:53:45 +0100

    Add missing ThreadPool.cpp file

---
+++ MonaBase/sources/ThreadPool.cpp
@@ -0,0 +1,41 @@
+/*
+This file is a part of MonaSolutions Copyright 2017
+mathieu.poux[a]gmail.com
+jammetthomas[a]gmail.com
+
+This program is free software: you can redistribute it and/or
+modify it under the terms of the the Mozilla Public License v2.0.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+Mozilla Public License v. 2.0 received along this program for more
+details (or else see http://mozilla.org/MPL/2.0/).
+
+*/
+
+#include "Mona/ThreadPool.h"
+
+using namespace std;
+
+
+namespace Mona {
+
+void ThreadPool::init(UInt16 threads, Thread::Priority priority) {
+	_threads.resize(_size = threads ? threads : Thread::ProcessorCount());
+	for (UInt16 i = 0; i < _size; ++i)
+		_threads[i].reset(new ThreadQueue("ThreadPool", priority));
+}
+
+UInt16 ThreadPool::join() {
+	UInt16 count(0);
+	for (unique<ThreadQueue>& pThread : _threads) {
+		if (!pThread->running())
+			continue;
+		++count;
+		pThread->stop();
+	}
+	return count;
+}
+
+} // namespace Mona
--- Version.h
+++ Version.h
@@ -1,1 +1,1 @@
-#define MONA_VERSION 180
+#define MONA_VERSION 177
